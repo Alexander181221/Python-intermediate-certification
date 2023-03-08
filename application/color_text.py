953d7d74b2d860d63f483fe3c2133a5cf98512dc
Alexander1812 <jayero@mail.ru>
2023-03-09 02:41:00 +0300

    color

---
+++ application/color_text.py
@@ -0,0 +1,14 @@
+def out_red(text):
+    print("{}" .format(text))
+
+
+def out_blue(text):
+    print("{}" .format(text))
+
+
+def out_yellow(text):
+    print(" {}" .format(text))
+
+
+def color_result(text):
+    print("{}" .format(text))
